1a14c415eafcbd83564bf62b7e431bc16532d316
Eduardo Patrocinio <epatro@amazon.com>
2024-04-11 11:28:25 -0400

    Create repeater (active-active cache) - 1 message /second

--- .aws-sam/build/RepeaterFunction/app.py
+++ .aws-sam/build/RepeaterFunction/app.py
@@ -1,13 +1,33 @@
 import json
 import time
-import urllib.request
 import json
-import os
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+kinesis_client = boto3.client("kinesis")
+stream_name = "lowd"
+logger = logging.getLogger(__name__)
+
+def put_record(count):
+    try:
+        response = kinesis_client.put_record(
+            StreamName=stream_name, Data=json.dumps("anything"), PartitionKey=str(count)
+        )
+        logger.info ("Put record %d in stream %s.", count, stream_name)
+        print ("Put record %d in stream." % count)
+    except ClientError:
+        logger.exception("Couldn't put record in stream %s", stream_name)
+        raise
+    else:
+        return response
 
 def lambda_handler(event, context):
 
     # Run for 5 minutes
+    counter = 0
     for iter in range(1, 5*60):
-        print("Sending")
+        put_record(counter)
         time.sleep(1)
+        counter = counter+1
     
