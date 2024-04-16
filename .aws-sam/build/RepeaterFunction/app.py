182fbbb89b00abd67142803d013b0a7cabdc8a73
Eduardo Patrocinio <epatro@amazon.com>
2024-04-16 13:15:43 -0300

    Vary key in put_record

--- .aws-sam/build/RepeaterFunction/app.py
+++ .aws-sam/build/RepeaterFunction/app.py
@@ -11,16 +11,17 @@ kinesis_client = boto3.client("kinesis")
 stream_name = "lowd"
 logger = logging.getLogger(__name__)
 tps = int(os.environ.get("TPS"))
+counter = 0
 
-def put_record(count):
+def put_record():
     try:
         for iter in range(1, tps):
-
             response = kinesis_client.put_record(
-                StreamName=stream_name, Data=json.dumps("anything"), PartitionKey=str(count)
+                StreamName=stream_name, Data=json.dumps("anything"), PartitionKey=str(counter)
             )
+            counter = counter+1
         logger.info ("Put %d records %d in stream %s.", tps, count, stream_name)
-        print ("Put %d record %d in stream." % (tps, count))
+        print ("Put %d record %d in stream." % (tps, counter))
     except ClientError:
         logger.exception("Couldn't put record in stream %s", stream_name)
         raise
@@ -31,14 +32,12 @@ def lambda_handler(event, context):
 
     print ("TPS: ", tps)
 
-    # Run for 5 minutes
-    counter = 0
-    for iter in range(1, 5*60):
+    # Run for 10 minutes
+    for iter in range(1, 10*60):
         start_time = time.time()
-        put_record(counter)
+        put_record()
         time_spent = time.time() - start_time
         print ("Time spent %s." % time_spent)
         if time_spent < 1:
             time.sleep(1 - time_spent)
-        counter = counter+1
     
